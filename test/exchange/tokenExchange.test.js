1375032dbb551814e037419ce62fb685b750bdd5
Jared Hanson <jaredhanson@gmail.com>
2016-09-20 11:43:33 -0700

    Initial commit.

---
+++ test/exchange/tokenExchange.test.js
@@ -0,0 +1,53 @@
+var chai = require('chai')
+  , tokenExchange = require('../../lib/exchange/tokenExchange');
+
+
+describe('exchange.tokenExchange', function() {
+  
+  it('should be named device_code', function() {
+    expect(tokenExchange(function(){}).name).to.equal('token_exchange');
+  });
+  
+  describe('issuing an access token', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, subjectToken, hints, done) {
+        if (client.id !== '1') { return done(new Error('incorrect client argument')); }
+        if (subjectToken !== 'accVkjcJyb4BWCxGsndESCJQbdFMogUC5PbRDqceLTC') { return done(new Error('incorrect subjectToken argument')); }
+        return done(null, '2YotnFZFEjr1zCsicMWpAA');
+      }
+      
+      chai.connect.use(tokenExchange(issue))
+        .req(function(req) {
+          req.user = { id: '1', name: 'Frontend Service' };
+          req.body = {
+            resource: 'https://backend.example.com/api',
+            subject_token: 'accVkjcJyb4BWCxGsndESCJQbdFMogUC5PbRDqceLTC',
+            subject_token_type: 'urn:ietf:params:oauth:token-type:access_token'
+          };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .next(function(err) {
+          console.log(err);
+          console.log(err.stack);
+          throw err;
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"2YotnFZFEjr1zCsicMWpAA","token_type":"Bearer","issued_token_type":"urn:ietf:params:oauth:token-type:access_token"}');
+    });
+  });
+  
+});
---
+++ test/package.test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+var pkg = require('..');
+var expect = require('chai').expect;
+
+
+describe('oauth2orize-device-code', function() {
+  
+  it('should export exchanges', function() {
+    expect(pkg.exchange).to.be.an('object');
+    expect(pkg.exchange.tokenExchange).to.be.a('function');
+  });
+  
+});
