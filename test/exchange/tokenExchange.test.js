76280b6b5553eb1bc868424c179f55becc017ba3
Jared Hanson <jaredhanson@gmail.com>
2017-09-14 16:39:55 -0700

    Delegation example.

--- test/exchange/tokenExchange.test.js
+++ test/exchange/tokenExchange.test.js
@@ -185,6 +185,50 @@ describe('exchange.tokenExchange', function() {
     });
   });
   
+  describe('delegation token exchange example from -09', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, subjectToken, scope, done) {
+        if (client.id !== '1') { return done(new Error('incorrect client argument')); }
+        if (subjectToken !== 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjE2In0.eyJhdWQiOiJodHRwczovL2FzLmV4YW1wbGUuY29tIiwiaXNzIjoiaHR0cHM6Ly9vcmlnaW5hbC1pc3N1ZXIuZXhhbXBsZS5uZXQiLCJleHAiOjE0NDE5MTAwNjAsInNjcCI6WyJzdGF0dXMiLCJmZWVkIl0sInN1YiI6InVzZXJAZXhhbXBsZS5uZXQiLCJtYXlfYWN0Ijp7InN1YiI6ImFkbWluQGV4YW1wbGUubmV0In19.ut0Ll7wm920VzRvuLGLFoPJLeO5DDElxsax1L_xKUm2eooiNSfuif-OGa2382hPyFYnddKIa0wmDhQksW018Rw') { return done(new Error('incorrect subjectToken argument')); }
+        if (scope !== undefined) { return done(new Error('incorrect scope argument')); }
+        
+        return done(null, 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjcyIn0.eyJhdWQiOiJ1cm46ZXhhbXBsZTpjb29wZXJhdGlvbi1jb250ZXh0IiwiaXNzIjoiaHR0cHM6Ly9hcy5leGFtcGxlLmNvbSIsImV4cCI6MTQ0MTkxMzYxMCwic2NwIjpbInN0YXR1cyIsImZlZWQiXSwic3ViIjoidXNlckBleGFtcGxlLm5ldCIsImFjdCI6eyJzdWIiOiJhZG1pbkBleGFtcGxlLm5ldCJ9fQ._qjM7Ij_HcrC78omT4jiZTFJOuzsAj1wPo31ymQS-Suqr64S1jCp6pfQR-in_OOAosAGamEg4jyPsht6kMAiYA', { issued_token_type: 'urn:ietf:params:oauth:token-type:jwt', expires_in: 3600 });
+      }
+      
+      chai.connect.use(tokenExchange(issue))
+        .req(function(req) {
+          req.user = { id: '1' };
+          req.body = {
+            audience: 'urn:example:cooperation-context',
+            subject_token: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjE2In0.eyJhdWQiOiJodHRwczovL2FzLmV4YW1wbGUuY29tIiwiaXNzIjoiaHR0cHM6Ly9vcmlnaW5hbC1pc3N1ZXIuZXhhbXBsZS5uZXQiLCJleHAiOjE0NDE5MTAwNjAsInNjcCI6WyJzdGF0dXMiLCJmZWVkIl0sInN1YiI6InVzZXJAZXhhbXBsZS5uZXQiLCJtYXlfYWN0Ijp7InN1YiI6ImFkbWluQGV4YW1wbGUubmV0In19.ut0Ll7wm920VzRvuLGLFoPJLeO5DDElxsax1L_xKUm2eooiNSfuif-OGa2382hPyFYnddKIa0wmDhQksW018Rw',
+            subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+            actor_token: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjE2In0.eyJhdWQiOiJodHRwczovL2FzLmV4YW1wbGUuY29tIiwiaXNzIjoiaHR0cHM6Ly9vcmlnaW5hbC1pc3N1ZXIuZXhhbXBsZS5uZXQiLCJleHAiOjE0NDE5MTAwNjAsInN1YiI6ImFkbWluQGV4YW1wbGUubmV0In0.7YQ-3zPfhUvzje5oqw8COCvN5uP6NsKik9CVV6cAOf4QKgM-tKfiOwcgZoUuDL2tEs6tqPlcBlMjiSzEjm3yBg',
+            actor_token_type: 'urn:ietf:params:oauth:token-type:jwt'
+          };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .next(function(err) {
+          throw err;
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"eyJhbGciOiJFUzI1NiIsImtpZCI6IjcyIn0.eyJhdWQiOiJ1cm46ZXhhbXBsZTpjb29wZXJhdGlvbi1jb250ZXh0IiwiaXNzIjoiaHR0cHM6Ly9hcy5leGFtcGxlLmNvbSIsImV4cCI6MTQ0MTkxMzYxMCwic2NwIjpbInN0YXR1cyIsImZlZWQiXSwic3ViIjoidXNlckBleGFtcGxlLm5ldCIsImFjdCI6eyJzdWIiOiJhZG1pbkBleGFtcGxlLm5ldCJ9fQ._qjM7Ij_HcrC78omT4jiZTFJOuzsAj1wPo31ymQS-Suqr64S1jCp6pfQR-in_OOAosAGamEg4jyPsht6kMAiYA","issued_token_type":"urn:ietf:params:oauth:token-type:jwt","expires_in":3600,"token_type":"Bearer"}');
+    });
+  });
+  
   describe('handling a request without subject_token parameter', function() {
     var err;
 
