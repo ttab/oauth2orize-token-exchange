8773a56f7dc34b9afef886ca83585a348e8fd6e6
Jared Hanson <jaredhanson@gmail.com>
2017-09-13 18:16:01 -0700

    More tests.

--- test/exchange/tokenExchange.test.js
+++ test/exchange/tokenExchange.test.js
@@ -14,7 +14,47 @@ describe('exchange.tokenExchange', function() {
     }).to.throw(TypeError, 'oauth2orize.tokenExchange exchange requires an issue callback');
   });
   
-  describe('issuing an access token based on audience', function() {
+  describe('issuing an access token', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, subjectToken, done) {
+        if (client.id !== '1') { return done(new Error('incorrect client argument')); }
+        if (subjectToken !== 'accVkjcJyb4BWCxGsndESCJQbdFMogUC5PbRDqceLTC') { return done(new Error('incorrect subjectToken argument')); }
+        
+        return done(null, '2YotnFZFEjr1zCsicMWpAA');
+      }
+      
+      chai.connect.use(tokenExchange(issue))
+        .req(function(req) {
+          req.user = { id: '1', name: 'Photo Printing' };
+          req.body = {
+            subject_token: 'accVkjcJyb4BWCxGsndESCJQbdFMogUC5PbRDqceLTC',
+            subject_token_type: 'urn:ietf:params:oauth:token-type:access_token'
+          };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .next(function(err) {
+          throw err;
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"2YotnFZFEjr1zCsicMWpAA","token_type":"Bearer","issued_token_type":"urn:ietf:params:oauth:token-type:access_token"}');
+    });
+  });
+  
+  describe('issuing an access token based on audience, specified using resource', function() {
     var response, err;
 
     before(function(done) {
